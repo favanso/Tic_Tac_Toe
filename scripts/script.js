c3bbee788b83c850e491244b6dbe6b1e22ed5f19
Fernando Branco <faavanso@gmail.com>
2022-03-25 00:57:25 -0400

    game without Robot and timer

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,21 @@
-const statusDisplay = document.getElementById("game");
-const turnDisplay = document.getElementById("turn");
-const gamePlayed = document.getElementById("gamePlayed");
-const PvP = document.getElementById("PvP");
-const PvC = document.getElementById("PvC");
+const winningMessage = () => `Player ${currentPlayer} has won!`;
+const drawMessage = () => `Game ended in a draw!`;
+const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
+let gameState = ["", "", "", "", "", "", "", "", ""];
+let currentPlayer = "X";
+// let gameActive = false;
+const winningConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
-let gamePlayedTimes = [];
-
-let gameActive = false;
 function start() {
-  handleRestartGame();
   if (document.getElementById("PvP").checked) {
     chose = "Player v Player. <br/>";
   } else {
@@ -24,113 +31,95 @@ function start() {
     "Starting a New Game. <br/>" + chose + currentPlayer + " starts First.";
 }
 
-function playAgain() {
-  document.getElementById("turn").innerHTML = "Press Start!";
-  document.getElementById("PvP").checked = true;
-  document.getElementById("playAgain").disabled = true;
+function restart() {
+  var txt;
+  if (
+    confirm("Do you want to play again? " + winningMessage() + drawMessage())
+  ) {
+    document.getElementById("game").innerHTML = "Game";
+    document.getElementById("turn").innerHTML = "Press Start!";
+    document.getElementById("PvP").checked = true;
+    // RESET TIMER
+    document.getElementById("0").innerHTML = "";
+    document.getElementById("1").innerHTML = "";
+    document.getElementById("2").innerHTML = "";
+    document.getElementById("3").innerHTML = "";
+    document.getElementById("4").innerHTML = "";
+    document.getElementById("5").innerHTML = "";
+    document.getElementById("6").innerHTML = "";
+    document.getElementById("7").innerHTML = "";
+    document.getElementById("8").innerHTML = "";
+    gameState = ["", "", "", "", "", "", "", "", ""];
+    txt = "Press Start button to Play";
+  } else {
+    txt = "Thank you for playing";
+  }
+  document.getElementById("game").innerHTML = txt;
 }
 
-let gameState = ["", "", "", "", "", "", "", "", ""];
-const winningConditions = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6],
-];
-
-const winningMessage = () => `Player ${currentPlayer} has won!`;
-const drawMessage = () => `Game ended in a draw!`;
-const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
-
 function cell0() {
-  if (document.getElementById("0").innerHTML == "" && gameActive) {
+  if (document.getElementById("0").innerHTML == "") {
     document.getElementById("0").innerHTML = currentPlayer;
     gameState[0] = currentPlayer;
     handleResultValidation();
   }
 }
 function cell1() {
-  if (document.getElementById("1").innerHTML == "" && gameActive) {
+  if (document.getElementById("1").innerHTML == "") {
     document.getElementById("1").innerHTML = currentPlayer;
     gameState[1] = currentPlayer;
     handleResultValidation();
   }
 }
 function cell2() {
-  if (document.getElementById("2").innerHTML == "" && gameActive) {
+  if (document.getElementById("2").innerHTML == "") {
     document.getElementById("2").innerHTML = currentPlayer;
     gameState[2] = currentPlayer;
     handleResultValidation();
   }
 }
 function cell3() {
-  if (document.getElementById("3").innerHTML == "" && gameActive) {
+  if (document.getElementById("3").innerHTML == "") {
     document.getElementById("3").innerHTML = currentPlayer;
     gameState[3] = currentPlayer;
     handleResultValidation();
   }
 }
 function cell4() {
-  if (document.getElementById("4").innerHTML == "" && gameActive) {
+  if (document.getElementById("4").innerHTML == "") {
     document.getElementById("4").innerHTML = currentPlayer;
     gameState[4] = currentPlayer;
     handleResultValidation();
   }
 }
 function cell5() {
-  if (document.getElementById("5").innerHTML == "" && gameActive) {
+  if (document.getElementById("5").innerHTML == "") {
     document.getElementById("5").innerHTML = currentPlayer;
     gameState[5] = currentPlayer;
     handleResultValidation();
   }
 }
 function cell6() {
-  if (document.getElementById("6").innerHTML == "" && gameActive) {
+  if (document.getElementById("6").innerHTML == "") {
     document.getElementById("6").innerHTML = currentPlayer;
     gameState[6] = currentPlayer;
     handleResultValidation();
   }
 }
 function cell7() {
-  if (document.getElementById("7").innerHTML == "" && gameActive) {
+  if (document.getElementById("7").innerHTML == "") {
     document.getElementById("7").innerHTML = currentPlayer;
     gameState[7] = currentPlayer;
     handleResultValidation();
   }
 }
 function cell8() {
-  if (document.getElementById("8").innerHTML == "" && gameActive) {
+  if (document.getElementById("8").innerHTML == "") {
     document.getElementById("8").innerHTML = currentPlayer;
     gameState[8] = currentPlayer;
     handleResultValidation();
   }
 }
-
-// document
-//   .querySelectorAll("cell")
-//   .forEach((cell) => cell.addEventListener("onClick", handleCellClick));
-
-// function handleCellClick(clickedCellEvent) {
-//     document.getElementById("turn").innerHTML = "HI"
-//   const clickedCell = clickedCellEvent.target;
-//   const clickedCellIndex = parseInt(clickedCell.getAttribute("id"));
-
-//   if (gameState[clickedCellIndex] !== "" || !gameActive) {
-//     return;
-//   }
-
-//   handleCellPlayed(clickedCell, clickedCellIndex);
-//   handleResultValidation();
-// }
-
-// function handleCellPlayed(clickedCell, clickedCellIndex) {
-//   gameState[clickedCellIndex] = currentPlayer;
-//   clickedCell.innerHTML = currentPlayer;
-// }
 
 function handleResultValidation() {
   let roundWon = false;
@@ -148,18 +137,12 @@ function handleResultValidation() {
     }
   }
   if (roundWon) {
-    document.getElementById("game").innerHTML = winningMessage();
-    gameActive = false;
-    document.getElementById("playAgain").disabled = false;
-    document.getElementById("turn").innerHTML = "Do you like to play again?";
+    restart();
     return;
   }
   let roundDraw = !gameState.includes("");
   if (roundDraw) {
-    document.getElementById("game").innerHTML = drawMessage();
-    gameActive = false;
-    document.getElementById("playAgain").disabled = false;
-    document.getElementById("turn").innerHTML = "Do you like to play again?";
+    restart();
     return;
   }
 
@@ -167,21 +150,12 @@ function handleResultValidation() {
 }
 
 function handlePlayerChange() {
-  //
   currentPlayer = currentPlayer === "X" ? "O" : "X";
 
   document.getElementById("turn").innerHTML = currentPlayerTurn();
   if (document.getElementById("PvC").checked && currentPlayer == "O") {
     setTimeout(Computer(), 2000);
   }
-}
-
-function handleRestartGame() {
-  gameActive = true;
-  //TIMER
-  currentPlayer = "X";
-  gameState = ["", "", "", "", "", "", "", "", ""];
-  document.querySelectorAll("cell").forEach((cell) => (cell.innerHTML = ""));
 }
 
 function Computer() {
